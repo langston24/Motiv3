bd0378a40170db169fa8008e7e12737662989751
langston24 <langston.muhammad@hgs.hiddengeniusproject.org>
2017-06-20 13:22:32 -0700

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by HGPMAC16 on 6/20/17.
+//  Copyright © 2017 HGPMAC16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
